a04318ff2aef114bd203f009d595e5bb6e59650e
Video <videogamesm12@gmail.com>
2023-07-25 14:07:31 -0600

    The mod now should (hopefully) support being run under a server

--- src/main/java/me/videogamesm12/cfx/CFX.java
+++ src/main/java/me/videogamesm12/cfx/CFX.java
@@ -2,11 +2,11 @@ package me.videogamesm12.cfx;
 
 import lombok.Getter;
 import me.videogamesm12.cfx.config.CFXConfig;
-import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.ModInitializer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class CFX implements ClientModInitializer
+public class CFX implements ModInitializer
 {
     @Getter
     private static final Logger logger = LogManager.getLogger("CFX");
@@ -15,7 +15,7 @@ public class CFX implements ClientModInitializer
     private static final CFXConfig config = CFXConfig.load();
 
     @Override
-    public void onInitializeClient()
+    public void onInitialize()
     {
     }
 }
