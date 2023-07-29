1494192fe2bf81e1239049a6fef22a79f69c3347
Video <videogamesm12@gmail.com>
2023-07-29 15:53:35 -0600

    WIP commit - doesn't fully work for some ridiculous reason

--- src/main/java/me/videogamesm12/cfx/config/CFXConfig.java
+++ src/main/java/me/videogamesm12/cfx/config/CFXConfig.java
@@ -34,6 +34,8 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 public class CFXConfig
@@ -41,7 +43,7 @@ public class CFXConfig
     private static final Gson gson = new GsonBuilder().setPrettyPrinting().create();
     private static final File file = new File(FabricLoader.getInstance().getConfigDir().toFile(), "cfx.json");
 
-    private static final int latestVersion = 2;
+    private static final int latestVersion = 3;
 
     public static CFXConfig load()
     {
@@ -93,6 +95,8 @@ public class CFXConfig
 
     private Text textPatches = new Text();
 
+    private Overrides overrides = new Overrides();
+
     public void save()
     {
         try (BufferedWriter writer = Files.newBufferedWriter(file.toPath()))
@@ -142,6 +146,17 @@ public class CFXConfig
     }
 
     @Getter
+    public static class Overrides
+    {
+        /**
+         * A list of forcibly disabled patch mixin names. This is usually for in case a patch is causing a serious
+         *  problem which can't be mitigated by disabling the patch traditionally (e.g. the client refuses to boot due
+         *  to the patch mixin itself being applied in scenarios where it shouldn't be).
+         */
+        private List<String> forciblyDisabledPatches = new ArrayList<>();
+    }
+
+    @Getter
     public static class Render
     {
         private Entities entity = new Entities();
@@ -170,9 +185,26 @@ public class CFXConfig
     @Getter
     public static class Text
     {
+        private ClickEventComponent clickEvent = new ClickEventComponent();
+
         private HoverEventComponent hoverEvent = new HoverEventComponent();
 
         private TranslatableComponent translation = new TranslatableComponent();
+
+        @Getter
+        @Setter
+        public static class ClickEventComponent
+        {
+            private CommandClickMode commandClickMode = CommandClickMode.NOTIFY;
+
+            public enum CommandClickMode
+            {
+                DO_NOTHING,     // Clicking the text will simply do nothing
+                NOTIFY,         // Clicking the text will cause a prompt to appear on the screen asking for confirmation
+                                //  before executing the command
+                VANILLA         // Clicking the text will execute the command like normally
+            }
+        }
 
         @Getter
         @Setter
