0724b5d9b9e9c8be17d7f4b86ab62f8f06e41a2e
Video <videogamesm12@gmail.com>
2023-08-02 20:00:35 -0600

    Bruh

---
+++ v1_14/src/main/java/me/videogamesm12/cfx/v1_14/sensitive_patches/ServerClickableCommand.java
@@ -0,0 +1,9 @@
+package me.videogamesm12.cfx.v1_14.sensitive_patches;
+
+import net.minecraft.block.entity.SignBlockEntity;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(SignBlockEntity.class)
+public class ServerClickableCommand
+{
+}
