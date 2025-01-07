5497bc849b3fda995e9ecc3570a6a0d14e94f3fd
Video <videogamesm12@gmail.com>
2025-01-06 23:01:46 -0700

    Fixes 1.20.2+ NBTSize patch not bypassing right

--- v1_20_2/src/main/java/me/videogamesm12/cfx/v1_20_2/patches/individual/NBTSize.java
+++ v1_20_2/src/main/java/me/videogamesm12/cfx/v1_20_2/patches/individual/NBTSize.java
@@ -4,6 +4,7 @@ import me.videogamesm12.cfx.CFX;
 import me.videogamesm12.cfx.management.PatchMeta;
 import net.minecraft.nbt.NbtTagSizeTracker;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -12,15 +13,18 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @PatchMeta(minVersion = 764, maxVersion = 999)
 public class NBTSize
 {
+    @Shadow private long allocatedBytes;
+
     @Inject(method = "add(J)V",
             at = @At(value = "INVOKE",
                     target = "Lnet/minecraft/nbt/NbtSizeValidationException;<init>(Ljava/lang/String;)V",
                     shift = At.Shift.BEFORE),
             cancellable = true)
-    public void disableNbtSizeRestrictions(long bits, CallbackInfo ci)
+    public void disableNbtSizeRestrictions(long bytes, CallbackInfo ci)
     {
         if (!CFX.getConfig().getNbtPatches().isSizeLimitEnabled())
         {
+            this.allocatedBytes += bytes;
             ci.cancel();
         }
     }
