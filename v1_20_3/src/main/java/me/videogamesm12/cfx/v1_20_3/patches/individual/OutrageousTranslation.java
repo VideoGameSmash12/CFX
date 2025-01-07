d197217e9afa42c567e35cf1ea74d26517d3bcdb
Video <videogamesm12@gmail.com>
2025-01-06 18:55:00 -0700

    Re-use existing placeholder pattern

--- v1_20_3/src/main/java/me/videogamesm12/cfx/v1_20_3/patches/individual/OutrageousTranslation.java
+++ v1_20_3/src/main/java/me/videogamesm12/cfx/v1_20_3/patches/individual/OutrageousTranslation.java
@@ -5,7 +5,9 @@ import me.videogamesm12.cfx.management.PatchMeta;
 import net.minecraft.text.Text;
 import net.minecraft.text.TranslatableTextContent;
 import net.minecraft.util.Language;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -20,7 +22,7 @@ import java.util.regex.Pattern;
 @PatchMeta(minVersion = 765, maxVersion = 765) // 1.20.3 to 1.20.4
 public class OutrageousTranslation
 {
-	private static final Pattern PLACEHOLDER_PATTERN = Pattern.compile("%([0-9]{1,}\\$)?s");
+	@Shadow @Final private static Pattern ARG_FORMAT;
 
 	@Inject(method = "of", at = @At("HEAD"), cancellable = true)
 	private static void injectOf(String key, Optional<String> fallback, Optional<List<Object>> args, CallbackInfoReturnable<TranslatableTextContent> cir)
@@ -45,7 +47,7 @@ public class OutrageousTranslation
 
 		// The key that would get processed
 		key = lang.get(key, fallback != null ? lang.get(fallback) : key);
-		Matcher matcher = PLACEHOLDER_PATTERN.matcher(key);
+		Matcher matcher = ARG_FORMAT.matcher(key);
 		amount += matcher.results().count();
 
 		// Arguments
